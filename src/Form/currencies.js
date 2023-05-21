2a6e1e302d3ac52eaa3d48c84c60d6713d874459
Alicja Orlik <88146004+Aliorl@users.noreply.github.com>
2023-05-21 19:51:36 +0200

    separate array with currencies added

---
+++ src/Form/currencies.js
@@ -0,0 +1,14 @@
+export const currencies = [
+    {
+        id: "PLN",
+        rate: 1,
+    },
+    {
+        id: "EUR",
+        rate: 4.5885,
+    },
+    {
+        id: "USD",
+        rate: 4.1612,
+    }
+];
