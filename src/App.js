3b4cbd69fb210d6518fef4e161be91d364a7b49e
Alicja Orlik <88146004+Aliorl@users.noreply.github.com>
2023-05-21 20:04:11 +0200

    Result fragment moved to the Form fragment

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import Container from "./Container";
 import Form from "./Form";
-import Result from "./Result";
 import Footer from "./Footer";
 
 function App() {
+
   return (
     <>
       <Container>
         <Form />
-        <Result />
       </Container>
       <Footer />
     </>
